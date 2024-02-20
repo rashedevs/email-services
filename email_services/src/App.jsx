73fd27b7700d70ec7b5c3a63f2df12e1ad0b8003
rashedevs <rasheduap2015@gmail.com>
2024-02-20 16:17:08 +0600

    first commit

---
+++ email_services/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./Components/Header/Header";
+import SideBar from "./Components/SideBar/SideBar";
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <Header></Header>
+        <SideBar></SideBar>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ email_services/src/Components/SideBar/SideBar.jsx
@@ -0,0 +1,9 @@
+const SideBar = () => {
+  return (
+    <div>
+      <h1>This is sidebar</h1>
+    </div>
+  );
+};
+
+export default SideBar;
